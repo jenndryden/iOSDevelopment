70c61026d6c9ec88c3ae88d08693476369420efe
Jennifer Dryden <jenndryden8@gmail.com>
2020-09-11 17:53:35 -0400

    Initial files 🎉
    Log-in screen and initial code

---
+++ iOSproject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOSproject
+//
+//  Created by Jennifer Dryden on 2020-09-11.
+//  Copyright © 2020 Jennifer Dryden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLbl: UILabel!
+    @IBOutlet weak var emailInput: UITextField!
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func begin(_ sender: Any) {
+    }
+    
+}
+
